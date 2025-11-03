43b4733bdeded978a7aabfbc56df431bf7b7bfa4
clearflameBY <55800505+clearflameBY@users.noreply.github.com>
2025-11-03 22:20:51 +0300

    Initial Commit

---
+++ akaSpotifyApp/akaSpotifyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  akaSpotifyAppApp.swift
+//  akaSpotifyApp
+//
+//  Created by Илья Степаненко on 3.11.25.
+//
+
+import SwiftUI
+
+@main
+struct akaSpotifyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
